6c45d7442d9968ce95d986e613fb81156272520e
huzaifa78613 <itsxhuxaifa@gmail.com>
2026-06-06 00:20:17 +0500

    fix: add SITE_URL to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const SITE_URL = process.env.NODE_ENV === 'production' 
+  ? 'https://exevolv.io' 
+  : 'http://localhost:3000';
